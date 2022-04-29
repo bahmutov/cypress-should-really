cf88e4afd0c446ef3d3294c059eec9a82cddf407
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2022-04-29 08:50:52 -0400

    add video and more comments [skip ci]

--- cypress/integration/filter-by-value/spec.js
+++ cypress/integration/filter-by-value/spec.js
@@ -1,6 +1,11 @@
 /// <reference types="cypress" />
 
-import { really, map, filter, its, isEqual } from '../../..'
+// normally you would import from "cypress-should-really"
+import { really, map, its, filter, isEqual } from '../../..'
+
+// find the explanation in the video
+// "Filter Input Elements By Value Using cypress-should-really Plugin"
+// https://youtu.be/Gxoo6uZMo9I
 
 it(
   'finds input elements with the current value "fox"',
@@ -13,11 +18,24 @@ it(
     cy.get('#inputs input[value=fox]').should('have.length', 1)
 
     // instead filter the elements by their current value
+    // first approach: use separate commands
+    // not ideal, since only the cy.filter is retried
+    // which loses the Cypress retry-ability
+    // https://on.cypress.io/retry-ability
+    cy.get('#inputs input')
+      .filter((k, el) => {
+        return el.value === 'fox'
+      })
+      // finds both input elements with the value "fox"
+      .should('have.length', 2)
+
+    // second approach: build up and use
+    // a single custom "should(callback)"
     cy.get('#inputs input').should(
+      // [jqueryElement]
       really(
-        // [jQueryElement]
         map(its('value')), // [string]
-        filter(isEqual('fox')), // filtered [string]
+        filter(isEqual('fox')),
         'have.length',
         2,
       ),
