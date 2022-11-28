30b22a3b0996352b9b2580a7d601d34a10899c79
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2022-11-28 10:54:17 -0500

    more visit updates

--- cypress/e2e/children/children-spec.js
+++ cypress/e2e/children/children-spec.js
@@ -7,7 +7,7 @@ describe(
   () => {
     // fails because the entire parent element is replaced
     it.skip('finds the children elements even if the entire element is replaced', () => {
-      cy.visit('cypress/integration/children/index.html')
+      cy.visit('cypress/e2e/children/index.html')
       cy.get('[data-cy=parent]')
         .should('be.visible')
         .contains('[data-cy=first]', 'First')
@@ -15,7 +15,7 @@ describe(
     })
 
     it('retries until the parent element has two children', () => {
-      cy.visit('cypress/integration/children/index.html')
+      cy.visit('cypress/e2e/children/index.html')
       // once we confirm the parent element has two children with data-cy attribute
       // we can continue
       cy.get('[data-cy=parent] [data-cy]').should('have.length', 2)
@@ -25,7 +25,7 @@ describe(
     })
 
     it('finds the children after wait', () => {
-      cy.visit('cypress/integration/children/index.html')
+      cy.visit('cypress/e2e/children/index.html')
         // let the page update itself including the parent element
         .wait(2000)
       cy.get('[data-cy=parent]')
@@ -34,14 +34,14 @@ describe(
     })
 
     it('really retries until the parent element has two children', () => {
-      cy.visit('cypress/integration/children/index.html')
+      cy.visit('cypress/e2e/children/index.html')
       cy.get('[data-cy=parent]')
         .should(really(invoke('find', '[data-cy]'), 'have.length', 2))
         .contains('[data-cy=first]', 'First')
     })
 
     it('really finds a child element with text', () => {
-      cy.visit('cypress/integration/children/index.html')
+      cy.visit('cypress/e2e/children/index.html')
       cy.get('[data-cy=parent]')
         // find the element with text "Second"
         // should exist
@@ -50,7 +50,7 @@ describe(
     })
 
     it('really finds text inside', () => {
-      cy.visit('cypress/integration/children/index.html')
+      cy.visit('cypress/e2e/children/index.html')
       cy.get('[data-cy=parent]')
         // take the text inside the element
         // find the word "Second"
